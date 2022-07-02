659a31043e093110284e2639909dccfbabc5218a
wendelsimoes <wendel.simoes.br@gmail.com>
2022-07-02 18:23:14 -0300

    feat: static add new list item

--- app.js
+++ app.js
@@ -6,27 +6,30 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = [];
+
 app.get("/", function (req, res) {
   const today = new Date();
-  const dayOfWeek = getDayOfWeek(today);
-  res.render("list", { currentDay: dayOfWeek });
+
+  options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  const dayOfWeek = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { currentDay: dayOfWeek, newItems: items });
+});
+
+app.post("/", function (req, res) {
+  items.push(req.body.newItem);
+
+  res.redirect("/");
 });
 
 app.listen(port, function () {
   console.log(`Server started on port ${port}`);
 });
-
-function getDayOfWeek(date) {
-  const dayOfWeek = new Date(date).getDay();
-  return isNaN(dayOfWeek)
-    ? null
-    : [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ][dayOfWeek];
-}
